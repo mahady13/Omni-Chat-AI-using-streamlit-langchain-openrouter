e54db6d752ee4d42fa844ac0924c11b9e586a0d8
mahady13 <mohiuddinmahady@gmail.com>
2026-07-27 07:22:59 +0600

    sorry,fixing langsmith mismatches again

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_PROJECT"] = "OmniChat-AI"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_openai import ChatOpenAI
@@ -7,10 +12,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
-os.environ["LANGCHAIN_PROJECT"] = "OmniChat-AI"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+
 
 st.set_page_config(page_title="OmniChat AI", page_icon="💬",layout="centered")
 st.title("OmniChat AI 💬")
